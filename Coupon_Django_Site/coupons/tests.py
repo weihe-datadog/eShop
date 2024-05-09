8d10eeeab98bb461c4d221b5ce54a5959a61bd27
Wei He <wei.he@datadoghq.com>
2024-05-09 17:48:15 -0400

    fix: add a test case

--- Coupon_Django_Site/coupons/tests.py
+++ Coupon_Django_Site/coupons/tests.py
@@ -1,3 +1,41 @@
-from django.test import TestCase
+import unittest
+from unittest.mock import MagicMock, patch
+from django.test import TestCase, Client
+from datetime import datetime, timedelta
+from django.http import request
+import json
 
-# Create your tests here.
+from django.urls import reverse
+
+class ApplyCouponTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    @patch('psycopg2.connect')
+    @patch('psycopg2.extras.NamedTupleCursor')
+    def test_apply_coupon(self, mock_cursor, mock_connect):
+        # Mocking the database connection
+        mock_conn = mock_connect.return_value
+        mock_cursor = mock_conn.cursor.return_value
+        mock_cursor.__enter__.return_value = mock_cursor
+
+        # Mocking coupon from database
+        # Assure to include future date to be valid coupon
+        mock_coupon = MagicMock(DiscountType='percentage',
+                                DiscountValue=25,
+                                ExpirationDate=datetime.now() + timedelta(days=10))
+        mock_cursor.fetchone.return_value = mock_coupon
+
+        # Provide the coupon and items information
+        coupon = {"coupon_code": "DISCOUNT25", "items": [{"id": "xxx", "name": "item1", "unit_price": 100, "units": 2}]}
+        
+        # response = self.client.post('/coupons/apply/', json.dumps(coupon), content_type='application/json')
+        response = self.client.post(reverse('coupons:apply'), json.dumps(coupon), content_type='application/json')
+
+        # Verify the response
+        self.assertEqual(response.status_code, 200)
+        response_json = response.json()
+        self.assertEqual(response_json['final_price'], 150)
+
+if __name__ == '__main__':
+    unittest.main()
--- Coupon_Django_Site/coupons/urls.py
+++ Coupon_Django_Site/coupons/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = 'coupons'
+
 urlpatterns = [
     path("", views.index, name="index"),
-    path("apply", views.apply_coupon, name="apply_coupon"),
+    path("apply", views.apply_coupon, name="apply"),
 ]
